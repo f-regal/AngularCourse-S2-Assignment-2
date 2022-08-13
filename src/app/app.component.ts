6712f45664fa8f4543c5a36354fa7a090f04a882
Faysal Regal <faysalregal@gmail.com>
2022-08-13 20:57:00 +0100

    Completed assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'assignment-2';
+  username = '';
+  disableReset = true;
+
+  onAllowReset() {
+    if(this.username.length > 0) {
+      this.disableReset = false;
+    }
+    return this.disableReset;
+  }
+
+  onReset() {
+    this.username = '';
+    this.disableReset = true;
+  }
 }
